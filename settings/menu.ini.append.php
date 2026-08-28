d4254f90443868eccc1720e17818ebec2d136d4e
7x <info@se7enx.com>
2026-08-28 09:29:34 -0700

    Removed: legacy "Exponential Layouts" top admin menu tab (kept the "Exponential Layouts UI" tab).

--- settings/menu.ini.append.php
+++ settings/menu.ini.append.php
@@ -1,39 +1,42 @@
 <?php /* #?ini charset="utf-8"?
 
-[NavigationPart]
-Part[ezexplayoutscorepart]=Exponential Layouts
+# The legacy "Exponential Layouts" top admin tab has been disabled in favor of
+# the "Exponential Layouts UI" tab from the explayouts_ui extension.
 
-[TopAdminMenu]
-Tabs[]=explayouts_dashboard
+#[NavigationPart]
+#Part[ezexplayoutscorepart]=Exponential Layouts
 
-[Topmenu_explayouts_dashboard]
-NavigationPartIdentifier=ezexplayoutscorepart
-Name=Exponential Layouts
-Tooltip=Exponential Layouts core admin
-URL[]
-URL[default]=explayouts/layout_list
-Enabled[]
-Enabled[default]=true
-Enabled[browse]=false
-Enabled[edit]=false
-Shown[]
-Shown[default]=true
-Shown[edit]=true
-Shown[navigation]=true
-Shown[browse]=true
-PolicyList[]=explayouts/read
+#[TopAdminMenu]
+#Tabs[]=explayouts_dashboard
 
-[Leftmenu_explayouts_dashboard]
-Name=Exponential Layouts
-Links[]
-LinkNames[]
-Links[Layouts]=explayouts/layout_list
-LinkNames[Layouts]=Layouts
-Links[Rules]=explayouts/rule_list
-LinkNames[Rules]=Rules
-Links[Template editor]=explayouts/template_editor
-LinkNames[Template editor]=Template editor
-Links[Setup]=explayouts/setup
-LinkNames[Setup]=Setup
+#[Topmenu_explayouts_dashboard]
+#NavigationPartIdentifier=ezexplayoutscorepart
+#Name=Exponential Layouts
+#Tooltip=Exponential Layouts core admin
+#URL[]
+#URL[default]=explayouts/layout_list
+#Enabled[]
+#Enabled[default]=true
+#Enabled[browse]=false
+#Enabled[edit]=false
+#Shown[]
+#Shown[default]=true
+#Shown[edit]=true
+#Shown[navigation]=true
+#Shown[browse]=true
+#PolicyList[]=explayouts/read
+
+#[Leftmenu_explayouts_dashboard]
+#Name=Exponential Layouts
+#Links[]
+#LinkNames[]
+#Links[Layouts]=explayouts/layout_list
+#LinkNames[Layouts]=Layouts
+#Links[Rules]=explayouts/rule_list
+#LinkNames[Rules]=Rules
+#Links[Template editor]=explayouts/template_editor
+#LinkNames[Template editor]=Template editor
+#Links[Setup]=explayouts/setup
+#LinkNames[Setup]=Setup
 
 */ ?>
