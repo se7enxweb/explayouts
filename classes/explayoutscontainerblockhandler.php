69fe39c9b3d91e7aa64583cd17c52ec1e9d5fdc2
7x <info@se7enx.com>
2026-08-28 00:47:43 -0700

    Updated: Add design parameters to container block handler so two_columns and column blocks expose View type, background, spacing, CSS and container options in the SPA sidebar.

--- classes/explayoutscontainerblockhandler.php
+++ classes/explayoutscontainerblockhandler.php
@@ -3,7 +3,94 @@ class expLayoutsContainerBlockHandler implements expLayoutsBlockHandlerInterface
 {
     public function getParameters()
     {
-        return array();
+        return array(
+            'background_color:enabled' => array(
+                'name' => 'Set background color',
+                'type' => 'compound_checkbox',
+                'default' => '',
+                'children' => array(
+                    'background_color:color' => array(
+                        'name' => 'Background color',
+                        'type' => 'select',
+                        'options' => array(
+                            'primary' => 'Primary (Yellow)',
+                            'secondary' => 'Secondary (Light gray)',
+                            'white' => 'White',
+                            'black' => 'Black',
+                        ),
+                        'default' => 'secondary',
+                    ),
+                ),
+            ),
+            'background_image:enabled' => array(
+                'name' => 'Set background image',
+                'type' => 'compound_checkbox',
+                'default' => '',
+                'children' => array(
+                    'background_image:image' => array(
+                        'name' => 'Background image',
+                        'type' => 'browse',
+                        'default' => '',
+                    ),
+                ),
+            ),
+            'vertical_whitespace:enabled' => array(
+                'name' => 'Use vertical whitespace',
+                'type' => 'compound_checkbox',
+                'default' => '1',
+                'children' => array(
+                    'vertical_whitespace:top' => array(
+                        'name' => 'On top',
+                        'type' => 'select',
+                        'options' => array(
+                            'none' => 'None',
+                            'small' => 'Small',
+                            'medium' => 'Medium',
+                            'large' => 'Large',
+                        ),
+                        'default' => 'none',
+                    ),
+                    'vertical_whitespace:bottom' => array(
+                        'name' => 'On bottom',
+                        'type' => 'select',
+                        'options' => array(
+                            'none' => 'None',
+                            'small' => 'Small',
+                            'medium' => 'Medium',
+                            'large' => 'Large',
+                        ),
+                        'default' => 'medium',
+                    ),
+                ),
+            ),
+            'css_class' => array(
+                'name' => 'CSS class',
+                'type' => 'string',
+                'default' => '',
+            ),
+            'css_id' => array(
+                'name' => 'CSS ID',
+                'type' => 'string',
+                'default' => '',
+            ),
+            'set_container' => array(
+                'name' => 'Wrap in container',
+                'type' => 'compound_checkbox',
+                'default' => '1',
+                'children' => array(
+                    'set_container:size' => array(
+                        'name' => 'Container size',
+                        'type' => 'select',
+                        'options' => array(
+                            '' => 'Regular',
+                            'narrow' => 'Narrow',
+                            'wide' => 'Wide',
+                        ),
+                        'default' => '',
+                    ),
+                ),
+            ),
+        );
     }
 
     public function getValues( $block )
